2f06785b6d28e8056f682b47d516a25b252cb2a0
Conrad Müller <conrad.mueller@andrena.de>
2011-09-28 17:12:42 +0200

    some message formatting for the spectator

--- client/swing-spectator/src/main/java/spectator/DataCollector.java
+++ client/swing-spectator/src/main/java/spectator/DataCollector.java
@@ -9,7 +9,7 @@ public class DataCollector implements MessageListener {
 
 	private StringBuilder currentRound = new StringBuilder();
 	private int currentRoundNumber;
-
+	
 	public DataCollector(RoundListener roundListener, ScoreListener scoreListener) {
 		this.roundListener = roundListener;
 		this.scoreListener = scoreListener;
@@ -21,13 +21,82 @@ public class DataCollector implements MessageListener {
 			currentRoundNumber = Integer.parseInt(parts[1]);
 			currentRound.setLength(0);
 		} else if (parts[0].equals("PLAYER LOST")) {
-			currentRound.append(message);
+			appendFormattedMessage(parts);
 			roundListener.roundCompleted(currentRoundNumber, currentRound.toString());
 		} else if (parts[0].equals("SCORE")) {
 			handleScores(message);
 		} else {
-			currentRound.append(message).append("\n");
+			appendFormattedMessage(parts);
 		}
+	}
+
+	private void appendFormattedMessage(String[] messageParts) {
+		String formatted = null;
+		if (messageParts[0].equals("ROUND STARTED")) {
+			String[] players = messageParts[1].split(",");
+			formatted = "Teilnehmer: " + germanJoin(players);
+		} else if (messageParts[0].equals("ANNOUNCED")) {
+			String player = messageParts[1];
+			String dice = formatDice(messageParts[2]);
+			formatted = player + " würfelt und sagt: " + dice;
+		} else if (messageParts[0].equals("PLAYER LOST")) {
+			String[] players = messageParts[1].split(",");
+			String reason = messageParts[2];
+			formatted = formatPlayerLost(players, reason);
+		} else if (messageParts[0].equals("ACTUAL DICE")) {
+			String dice = formatDice(messageParts[1]);
+			formatted = "Die Würfel werden aufgedeckt: " + dice;
+		}
+		if (formatted != null) {
+			currentRound.append(formatted);
+		} else {
+			currentRound.append(messageParts[0]);
+		}
+		currentRound.append("\n");
+	}
+
+	private String formatDice(String diceString) {
+		String[] dieStrings = diceString.split(",");
+		int die1 = Integer.parseInt(dieStrings[0]);
+		int die2 = Integer.parseInt(dieStrings[1]);
+		if (die1 == 2 && die2 == 1) {
+			return "Mäxchen!";
+		}
+		if (die1 == die2) {
+			switch (die1) {
+				case 1: return "Einserpasch";
+				case 2: return "Zweierpasch";
+				case 3: return "Dreierpasch";
+				case 4: return "Viererpasch";
+				case 5: return "Fünferpasch";
+				case 6: return "Sechserpasch";
+			}
+		}
+		return "" + die1 + die2;
+	}
+
+	private String formatPlayerLost(String[] players, String reason) {
+		String result;
+		if (players.length == 1) {
+			result = players[0] + " verliert";
+		} else {
+			result = germanJoin(players) + " verlieren";
+		}
+		return result + " (" + reason + ")";
+	}
+
+	private String germanJoin(String[] parts) {
+		if (parts.length == 0) {
+			return "";
+		}
+		String result = parts[0];
+		if (parts.length == 1) {
+			return result;
+		}
+		for (int i = 1; i < parts.length - 1; i++) {
+			result += ", " + parts[i];
+		}
+		return result + " und " + parts[parts.length - 1];
 	}
 
 	private void handleScores(String message) {
