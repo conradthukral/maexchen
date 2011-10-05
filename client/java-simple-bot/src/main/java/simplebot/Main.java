b1f6b0e1fce0f0728286fd812b1d5bfab47b66fe
Conrad Müller <conrad.mueller@andrena.de>
2011-10-05 19:22:23 +0200

    Can now choose between simple and random bot

--- client/java-simple-bot/src/main/java/simplebot/Main.java
+++ client/java-simple-bot/src/main/java/simplebot/Main.java
@@ -3,19 +3,36 @@ package simplebot;
 import java.io.IOException;
 
 import udphelper.MessageListener;
+import udphelper.MessageSender;
 import udphelper.UdpCommunicator;
 
 public class Main {
 
+	private enum BotTypes {
+		simple,
+		random
+	}
+	
 	public static void main(String[] args) throws IOException {
 		String serverHost = args[0];
 		int serverPort = Integer.parseInt(args[1]);
 		String clientName = args[2];
+		BotTypes botType = BotTypes.simple;
+		if (args.length > 3) {
+			botType = BotTypes.valueOf(args[3]);
+		}
 		
 		UdpCommunicator communicator = new UdpCommunicator(serverHost, serverPort);
-		MessageListener bot = new RandomBot(clientName, communicator.getMessageSender());
+		MessageListener bot = createBot(botType, clientName, communicator.getMessageSender());
 		communicator.addMessageListener(bot);
 		communicator.listenForMessages();
+	}
+
+	private static MessageListener createBot(BotTypes botType, String clientName, MessageSender messageSender) {
+		if (botType == BotTypes.random) {
+			return new RandomBot(clientName, messageSender);
+		}
+		return new SimpleBot(clientName, messageSender);
 	}
 
 }
